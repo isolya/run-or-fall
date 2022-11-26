293b01adaa512cd5717f2cfc37d2e3dcfc7b6542
Raevski Anatoly <rae2002future@yandex.ru>
2022-11-26 23:34:45 +0300

    лестница

--- script.js
+++ script.js
@@ -15,35 +15,30 @@ function start(x){
         document.querySelector('.page#timer').classList.remove('active');
         document.querySelector('.page#main').classList.add('active');
         
-        let div = document.createElement("div")
-        div.classList.add("bigblock"); 
-        let style = div.style
-        style.gridColumnStart = 1;
-        style.gridColumnEnd = 2;
-        style.gridRowStart = 1;
-        style.gridRowEnd = 4;
-        document.getElementById("main").appendChild(div);
-
-
-        //generateField();
-
+        generateField();
     }
     else{
         timer=setTimeout(start,1000, x - 1);
     }
 }
 
+function placeDiv(x1, y1, x2, y2){
+    let div = document.createElement("div")
+    let style = div.style
+    style.gridColumnStart = x1;
+    style.gridColumnEnd = x2;
+    style.gridRowStart = y1;
+    style.gridRowEnd = y2;
+    div.classList.add("bigblock")
+    return div
+}
+
 function generateField(){
     let columns = 10;
     let rows = 10;
-    
-    for( i = 0; i < 100; i++){
-       var row = document.createElement('div');
-       
-       document.getElementById("main").appendChild(row);
-      
-       row.classList.add("elem"); 
-        
-        
+    let grid = document.getElementById("main")
+    for( i = 1; i < 10; i++){
+       let div = placeDiv(i, i, i+2, i)
+       grid.appendChild(div)
     }
 }
