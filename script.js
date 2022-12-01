be7db79b3b318551504d9a76955094b8a22d265b
olya <osedysheva4@gmail.com>
2022-12-01 13:31:22 +0300

    gameOver

--- script.js
+++ script.js
@@ -6,19 +6,43 @@ document.querySelector('div.start').addEventListener('click', () => {
 });
 
 let timer;
-let timeScore;
+let score = 0;
 const coinsPoints = [
     [5, 3],
     [5, 4],
     [5, 6],
     [5, 8],
     [5, 9],
+    // [1, 1],
+    // [1, 2],
+    // [1, 3],
+    // [1, 4],
+    // [1, 5],
+
+]
+const bridgePoints = [
+    [2, 1],
+    [3, 1],
+    [4, 1],
+    [5, 1],
+    [6, 1],
+    [7, 1],
     [1, 1],
     [1, 2],
     [1, 3],
     [1, 4],
     [1, 5],
-
+    [1, 6],
+    [7, 2],
+    [7, 3],
+    [6, 3],
+    [5, 3],
+    [5, 4],
+    [5, 5],
+    [5, 6],
+    [5, 7],
+    [5, 8],
+    [5, 9]
 ]
 
 const state = {
@@ -28,11 +52,10 @@ const state = {
     direction: [-1, 0],
     duckCoord: {
         c: 1,
-        r: 8
+        r: 4
 
     }
 };
-
 
 const start = (x) => {
     document.getElementById('time').innerHTML = x;
@@ -78,64 +101,31 @@ const mountField = () => {
         grid.appendChild(cell.element);
     });
 }
+
 const renderCoins = () => {
     coinsPoints.forEach(([columnIndex, rowIndex]) => {
         state.cells[rowIndex * state.columnNumber + columnIndex].isCoins = true;
         state.cells.forEach((cell) => {
-
-
             if (cell.isCoins) {
                 cell.element.classList.add('is-coins');
             }
-
         });
     })
-
 }
+
 const generateBridge = () => {
-    const bridgePoints = [
-        [2, 1],
-        [3, 1],
-        [4, 1],
-        [5, 1],
-        [6, 1],
-        [7, 1],
-        [1, 1],
-        [1, 2],
-        [1, 3],
-        [1, 4],
-        [1, 5],
-        [1, 6],
-        [7, 2],
-        [7, 3],
-        [6, 3],
-        [5, 3],
-        [5, 4],
-        [5, 5],
-        [5, 6],
-        [5, 7],
-        [5, 8],
-        [5, 9]
-    ]
 
     bridgePoints.forEach(([columnIndex, rowIndex]) => {
         state.cells[rowIndex * state.columnNumber + columnIndex].isBridge = true;
-
     })
-
 }
+
 const render = () => {
-
     state.cells.forEach((cell) => {
-
         if (cell.isBridge) {
             cell.element.classList.add('is-bridge');
         }
-
-
     });
-
-
 }
 
 function duckCurrent() {
@@ -143,83 +133,63 @@ function duckCurrent() {
 }
 
 const moveDuck = () => {
-
-    duckCurrent()
-
-    if (state.duckCoord.c > 0 && state.duckCoord.c < 9 && state.duckCoord.r > 0 && state.duckCoord.r < 9) {
+    if (state.duckCoord.c >= 0 && state.duckCoord.c <= 9 && state.duckCoord.r >= 0 && state.duckCoord.r <= 9) {
         duckCurrent().classList.remove('cell--active');
         state.duckCoord.r += state.direction[0]
         state.duckCoord.c += state.direction[1]
+        // console.log(duckCurrent(), state.duckCoord);
         duckCurrent().classList.add('cell--active');
-
-
+        duckDirection();
     }
     removeCoin();
+    gameOver()
 }
 
-const duckMovement = () => {
 
-
-    duckCurrent().classList.add('cell--active');
-
+const duckDirection = () => {
     window.addEventListener('keydown', function (event) {
-
-        if (state.duckCoord.c > 0 && event.key == 'ArrowLeft') {
-
+        if (event.key == 'ArrowLeft') {
             state.direction = [0, -1]
-
-        } else if (state.duckCoord.r > 0 && event.key == 'ArrowUp') {
-
+        } else if (event.key == 'ArrowUp') {
             state.direction = [-1, 0]
         }
-        else if (state.duckCoord.c < 9 && event.key == 'ArrowRight') {
-
+        else if ( event.key == 'ArrowRight') {
             state.direction = [0, 1]
         }
-        else if (state.duckCoord.r < 9 && event.key == 'ArrowDown') {
+        else if ( event.key == 'ArrowDown') {
             state.direction = [1, 0]
-
         }
-
     });
+
 }
 
 const removeCoin = () => {
-
-    coinsPoints.forEach(coin => {
-const [connsC, coinsR] = coin;
-if(state.duckCoord.c === coin[0] && state.duckCoord.r === coin[1]){
-    
-    duckCurrent().classList.remove('is-coins');
-    score(0);
-
-    console.log(score);
-}
-
-});
-
-
-}
-const score = (s) => {
-    document.getElementById('score').innerHTML = s;
-   
-    timeScore = setTimeout(score, 1000, s + 1);
-
-
-
-
-    
+    for (let i = 0 ; i < coinsPoints.length; i++) {
+        const [coinC, coinR] = coinsPoints[i];
+        if (state.duckCoord.c === coinC && state.duckCoord.r === coinR) {
+            duckCurrent().classList.remove('is-coins');
+            coinsPoints.splice(i, 1);
+            score += 1;
+            document.getElementById('score').innerHTML = score; 
+        } 
+       
     }
-
-
-
-
+}
+ const gameOver = () => {
+    for (let b = 0 ; b < bridgePoints.length; b++) {
+        const [bridgeC, bridgeR] = bridgePoints[b];
+            if(state.duckCoord.c === bridgeC && state.duckCoord.r === bridgeR){
+            console.log('f')
+        // document.querySelector('.page#main').classList.remove('active');
+        // document.querySelector('.page#game-over').classList.add('active');
+    }
+}}
 const tick = () => {
-
-    moveDuck();
     
+    moveDuck();
     render();
-    setTimeout(tick, 1000);
+    
+     setTimeout(tick, 1000);
 }
 
 const initListeners = () => {
@@ -233,7 +203,7 @@ const startGame = () => {
     renderCoins();
     mountField();
     tick();
-    duckMovement();
+    duckDirection();
 }
 
 const main = () => {
